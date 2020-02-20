27626aabe8a027ebca85fa1f42e09796fb9cdd79
leeshen64 <lee_shen@sercomm.com>
2020-02-20 16:30:05 +0800

    Update hello.go

--- hello.go
+++ hello.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	who := "World!"
+	who := "World! And I did a modification"
 	if len(os.Args) > 1 { /* os.Args[0] is "hello" or "hello.exe" */
 		who = strings.Join(os.Args[1:], " ")
 	}
